5fff488c61ad2c293e756fa493c16e18a2ffd344
Andrew Marmion <andrew.marmion@gmail.com>
2019-11-11 18:33:37 +0000

    Add quotation marks

--- step.sh
+++ step.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -e $fdu_fabric_location ]; then
+if [ ! -e "$fdu_fabric_location" ]; then
   echo "Please provide the location of Fabric"
   exit 1
 fi
 
-if [ ! -e $fdu_google_services_location ]; then
+if [ ! -e "$fdu_google_services_location" ]; then
   echo "Please provide the location of your GoogleService-Info.plist"
   exit 1
 fi
@@ -15,4 +15,4 @@ if [ ! -e $fdu_dsym_location ]; then
   exit 1
 fi
 
-$fdu_fabric_location -gsp $fdu_google_services_location -p ios $fdu_dsym_location
+"$fdu_fabric_location" -gsp "$fdu_google_services_location" -p ios "$fdu_dsym_location"
