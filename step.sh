5eabe4a24ad15ae5d0961af424fefca9d0afa5d3
Andrew Marmion <andrewmarmion@users.noreply.github.com>
2020-03-20 06:18:03 +0000

    Add Logging

--- step.sh
+++ step.sh
@@ -5,14 +5,27 @@ if [ ! -e "$fdu_fabric_location" ]; then
   exit 1
 fi
 
+if [ "$fdu_logging" = "yes" ]; then
+  echo "Location of Fabric is: $fdu_fabric_location"
+fi
+
 if [ ! -e "$fdu_google_services_location" ]; then
   echo "Please provide the location of your GoogleService-Info.plist"
   exit 1
 fi
 
+if [ "$fdu_logging" = "yes" ]; then
+  echo "Location of the GoogleService-Info.plist is: $fdu_google_services_location"
+fi
+
 if [ ! -e "$fdu_dsym_location" ]; then
   echo "Please provide the location of your dSYM"
   exit 1
+fi
+
+if [ "$fdu_logging" = "yes" ]; then
+  echo "Location of the dSYM location is: $fdu_dsym_location"
+  echo
 fi
 
 "$fdu_fabric_location" -gsp "$fdu_google_services_location" -p ios "$fdu_dsym_location"
