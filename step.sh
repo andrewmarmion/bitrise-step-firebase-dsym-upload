965c44b34d01ca83648c3ffc1e74669ac57fecc8
Andrew Marmion <andrew.marmion@gmail.com>
2020-05-20 08:28:37 +0100

    fix(step): Fixes possible step failure
    Put back the default location

--- step.sh
+++ step.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo -e "\033[33mFabric is being deprecated November 15 2020.\033[0m"
+echo -e "\033[33mMake sure you update your dependencies to use FirebaseCrashlytics before then.\033[0m"
+
 if [ ! -e "$fdu_fabric_location" ]; then
   echo "Please provide the location of Fabric or FirebaseCrashlytics"
   exit 1
