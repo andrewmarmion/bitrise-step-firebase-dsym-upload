7f6d8635ecc535539ec63ff37eca28dec038dfdf
Andrew Marmion <andrew.marmion@gmail.com>
2022-07-26 12:30:46 +0100

    Update copy

--- step.sh
+++ step.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
-
-echo -e "\033[33mFabric is being deprecated November 15 2020.\033[0m"
-echo -e "\033[33mMake sure you update your dependencies to use FirebaseCrashlytics before then.\033[0m"
+echo -e "\033[33mThis step is for use with FirebaseCrashlytics that has been installed by CocoaPods.\033[0m"
+echo ""
 
 if [ ! -e "$fdu_fabric_location" ]; then
-  echo "Please provide the location of Fabric or FirebaseCrashlytics"
+  echo -e "\033[33mPlease provide the location of your CocoaPod installation of FirebaseCrashlytics\033[0m"
+  echo ""
   exit 1
 fi
 
 if [ "$fdu_logging" = "yes" ]; then
-  echo "Location of Fabric or FirebaseCrashlytics is: $fdu_fabric_location"
+  echo "Location of FirebaseCrashlytics is: $fdu_fabric_location"
+  echo ""
 fi
 
 if [ ! -e "$fdu_google_services_location" ]; then
-  echo "Please provide the location of your GoogleService-Info.plist"
+  echo -e "\033[33mPlease provide the location of your GoogleService-Info.plist\033[0m"
+  echo ""
   exit 1
 fi
 
 if [ "$fdu_logging" = "yes" ]; then
   echo "Location of the GoogleService-Info.plist is: $fdu_google_services_location"
+  echo ""
 fi
 
 if [ ! -e "$fdu_dsym_location" ]; then
-  echo "Please provide the location of your dSYM"
+  echo -e "\033[33mPlease provide the location of your dSYM\033[0m"
+  echo ""
   exit 1
 fi
 
 if [ "$fdu_logging" = "yes" ]; then
   echo "Location of the dSYM location is: $fdu_dsym_location"
+  echo ""
   echo
 fi
 
